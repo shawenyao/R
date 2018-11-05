c7082e6308f46677a0d07a823496749c980db133
Wenyao <38875149+shawenyao@users.noreply.github.com>
2018-11-04 23:51:17 -0800

    update example plot

--- main/smooth_path.R
+++ main/smooth_path.R
@@ -13,30 +13,31 @@ path <- data.frame(
   y = c(3, 4, 2, 1.5, 4, 6, 7)
 )
 
+#==== plot 1 ====
 plot1 <- bind_rows(
   path %>% 
-    mutate(iteration = "original"),
+    mutate(iteration = "Original"),
   path %>% 
     smooth_path_double() %>% 
     smooth_path_double() %>% 
-    mutate(iteration = "smoothed")
+    mutate(iteration = "Smoothed")
 ) %>% 
   ggplot(aes(x = x, y = y)) +
   geom_path(size = 3, linejoin = "round", lineend = "round") +
-  geom_point(size = 8, aes(color = iteration)) +
-  scale_color_manual(values = c("original" = "#F8766D", "smoothed" = "#619CFF")) +
-  scale_x_continuous(breaks = seq(min(path$x), max(path$x), 1), limits = c(min(path$x) - 0.4, max(path$x) + 0.4)) +
-  scale_y_continuous(breaks = seq(min(path$y), max(path$y), 1), limits = c(min(path$y) - 0.4, max(path$y) + 0.4)) +
+  geom_point(size = 6, aes(color = iteration)) +
+  scale_color_manual(values = c("Original" = "#F8766D", "Smoothed" = "#619CFF")) +
+  scale_x_continuous(breaks = seq(min(path$x), max(path$x), 1), limits = c(min(path$x) - 0.6, max(path$x) + 0.6)) +
+  scale_y_continuous(breaks = seq(min(path$y), max(path$y), 1), limits = c(min(path$y) - 0.6, max(path$y) + 0.6)) +
   coord_fixed() +
   facet_wrap(iteration~.) +
   theme_minimal() +
   theme(legend.position = "none", axis.text = element_blank(), strip.text = element_blank()) +
   labs(x = "", y = "")
 
-save_svg(plot = plot1, file_name = "output/plot1.svg", width = 8, height = 4)
+save_svg(plot = plot1, file_name = "output/plot1.svg", width = 6, height = 3)
 
-
-bind_rows(
+#==== final example ====
+plot_example <- bind_rows(
   path %>% 
     mutate(iteration = "Original", type = "1-way"),
   path %>% 
@@ -73,11 +74,15 @@ bind_rows(
   ggplot(aes(x = x, y = y)) +
   geom_path(size = 3, linejoin = "round", lineend = "round") +
   geom_point(size = 6, aes(color = iteration)) +
-  facet_grid(type~iteration) +
+  facet_grid(type~iteration, switch = "y") +
+  scale_x_continuous(breaks = seq(min(path$x), max(path$x), 1), limits = c(min(path$x) - 0.6, max(path$x) + 0.6)) +
+  scale_y_continuous(breaks = seq(min(path$y), max(path$y), 1), limits = c(min(path$y) - 0.6, max(path$y) + 0.6)) +
+  coord_fixed() +
   theme_minimal() +
-  theme(legend.position = "none") +
-  labs(x = "", y = "") +
-  coord_fixed()
+  theme(legend.position = "none", axis.text = element_blank(), strip.text = element_text(size = 20)) +
+  labs(x = "", y = "")
+
+save_svg(plot = plot_example, file_name = "output/plot_example.svg", width = 10, height = 10)
 
 # play sound when finished
 beep(sound = 2)
