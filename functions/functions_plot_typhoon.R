1986b3f81b6cb2312303f85fd24ebc2b6fad93ab
Wenyao <38875149+shawenyao@users.noreply.github.com>
2018-11-08 01:27:07 -0800

    update plots

---
+++ functions/functions_plot_typhoon.R
@@ -0,0 +1,38 @@
+#' plot typhoon graph
+#'
+#' @param iteration number of iterations
+#' @param total total number of paths per iteration
+#' 
+plot_typhoon <- function(iteration, total){
+  
+  data <- seq_len(iteration) %>% 
+    map(function(i){
+      seq_len(total) %>% 
+        map(function(j){
+          tibble(
+            id = j + (i - 1) * total,
+            theta = seq(from = sample(3:10, size = 1), to = sample(11:30, size = 1), length.out = 1000)
+          ) %>% 
+            mutate(
+              r = 1 / theta,
+              x = r * cos(theta + 2 * pi * (j + rnorm(1, sd = 0.2)) / total) + rnorm(1, sd = 0.005),
+              y = r * sin(theta + 2 * pi * (j + rnorm(1, sd = 0.2)) / total) + rnorm(1, sd = 0.005)
+            )
+        }) %>% 
+        bind_rows()
+    }) %>% 
+    bind_rows()
+  
+  typhoon_plot <- ggplot(data, aes(x = x, y = y, group = id)) +
+    geom_path(size = 0.4, lineend = "round", alpha = 0.2) +
+    coord_fixed() +
+    theme_minimal() +
+    theme(
+      legend.position = "none", 
+      axis.line = element_blank(),
+      axis.text = element_blank(), 
+      strip.text = element_blank(),
+      panel.grid = element_blank()
+    ) +
+    labs(x = "", y = "")
+}
--- main/typhoon_plot.R
+++ main/typhoon_plot.R
@@ -2,83 +2,28 @@ library(tidyverse)
 library(beepr)
 
 setwd("C:/Users/Wenyao/Desktop/R/R/")
+source("./functions/functions_plot_typhoon.R")
 source("./functions/functions_save_svg.R")
 source("./functions/functions_save_png.R")
 
-set.seed(1)
-
-iteration <- 2
-total <- 30
-data <- seq_len(iteration) %>% 
-  map(function(i){
-    seq_len(total) %>% 
-      map(function(j){
-        tibble(
-          id = j + (i - 1) * total,
-          theta = seq(from = sample(3:10, size = 1), to = sample(11:30, size = 1), length.out = 1000)
-        ) %>% 
-          mutate(
-            r = 1 / theta,
-            x = r * cos(theta + 2 * pi * (j + rnorm(1, sd = 0.2)) / total) + rnorm(1, sd = 0.005),
-            y = r * sin(theta + 2 * pi * (j + rnorm(1, sd = 0.2)) / total) + rnorm(1, sd = 0.005)
-          )
-      }) %>% 
-      bind_rows()
-  }) %>% 
-  bind_rows()
-
-plot1 <- ggplot(data, aes(x = x, y = y, group = id)) +
-  geom_path(size = 0.4, lineend = "round", alpha = 0.2) +
-  coord_fixed() +
-  theme_minimal() +
-  theme(
-    legend.position = "none", 
-    axis.line = element_blank(),
-    axis.text = element_blank(), 
-    strip.text = element_blank(),
-    panel.grid = element_blank()
-  ) +
-  labs(x = "", y = "")
-
-save_svg(plot = plot1, file_name = "output/typhoon/typhoon.svg", width = 10, height = 10)
-
 
 set.seed(1)
 
-iteration <- 10
-total <- 60
-data <- seq_len(iteration) %>% 
-  map(function(i){
-    seq_len(total) %>% 
-      map(function(j){
-        tibble(
-          id = j + (i - 1) * total,
-          theta = seq(from = sample(3:10, size = 1), to = sample(11:30, size = 1), length.out = 1000)
-        ) %>% 
-          mutate(
-            r = 1 / theta,
-            x = r * cos(theta + 2 * pi * (j + rnorm(1, sd = 0.3)) / total) + rnorm(1, sd = 0.0075),
-            y = r * sin(theta + 2 * pi * (j + rnorm(1, sd = 0.3)) / total) + rnorm(1, sd = 0.0075)
-          )
-      }) %>% 
-      bind_rows()
-  }) %>% 
-  bind_rows()
-
-plot2 <- ggplot(data, aes(x = x, y = y, group = id)) +
-  geom_path(size = 0.4, lineend = "round", alpha = 0.2) +
-  coord_fixed() +
-  theme_minimal() +
-  theme(
-    legend.position = "none", 
-    axis.line = element_blank(),
-    axis.text = element_blank(), 
-    strip.text = element_blank(),
-    panel.grid = element_blank()
-  ) +
-  labs(x = "", y = "")
-
-save_png(plot = plot2, print_plot = TRUE, file_name = "output/typhoon/typhoon2.png", width = 880, height = 880)
+list(
+  plot_id = c(1:3),
+  iteration = c(1, 5, 10),
+  total = c(30, 45, 60)
+) %>% 
+  pmap(
+    function(plot_id, iteration, total){
+      plot_typhoon(iteration = iteration, total = total) %>% 
+        save_png(
+          file_name = paste0("output/typhoon/typhoon", plot_id, ".png"),
+          width = 880, 
+          height = 880
+        )
+    }
+  )
 
 # play sound when finished
 beep(sound = 2)
