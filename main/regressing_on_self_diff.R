0f620ff7b675cd7afe27f1da13db3660cb59a9e1
Wenyao <38875149+shawenyao@users.noreply.github.com>
2018-12-12 23:07:06 -0800

    Update regressing_on_self_diff.R

--- main/regressing_on_self_diff.R
+++ main/regressing_on_self_diff.R
@@ -8,7 +8,7 @@ n <- 10000
 series <- runif(n+1)
 
 # y(t) - y(t-1) ~ y(t)
-y <- series[-1]-series[-length(series)]
+y <- series[-1] - series[-length(series)]
 x <- series[-length(series)]
 model <- lm(y~x)
 # RSQ ~ 0.5
@@ -21,8 +21,8 @@ n <- 10000
 series <- runif(n+2)
 
 # y(t) - y(t-1) - y(t-2) ~ y(t)
-y <- series[-c(1:2)]-
-  series[-c(length(series),length(series)-1)]-
+y <- series[-c(1:2)] -
+  series[-c(length(series),length(series)-1)] -
   series[-c(length(series),1)]
 x <- series[-c(length(series),length(series)-1)]
 model <- lm(y~x)
