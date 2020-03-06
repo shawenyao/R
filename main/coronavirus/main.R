7420f1086d80730796e982d7c30ff1c66c595e00
Wenyao <38875149+shawenyao@users.noreply.github.com>
2020-03-05 21:39:02 -0500

    Update main.R

--- main/coronavirus/main.R
+++ main/coronavirus/main.R
@@ -49,7 +49,7 @@ coronavirus <- coronavirus_raw %>%
 #===== plot on map =====
 # initialize
 output <- leaflet(
-  options = leafletOptions(minZoom = 3, maxZoom = 8)
+  options = leafletOptions(minZoom = 3, maxZoom = 7.5)
 ) %>% 
   addTiles() %>% 
   setView(lng = 120, lat = 35, zoom = 4) %>% 
