0e786415ae6187ddd021c78d1cc68b20a94306f1
Wenyao <38875149+shawenyao@users.noreply.github.com>
2020-03-15 20:08:43 -0400

    update plot

--- main/coronavirus/js_code.R
+++ main/coronavirus/js_code.R
@@ -15,7 +15,7 @@ for(i in 1:(length(all_dates))){
     
     # additional wait on the last frame
     js_code <- js_code %>% paste0(
-      "await new Promise(r => setTimeout(r, 2000));",
+      "await new Promise(r => setTimeout(r, 1000));",
       "if(count == 3 - 1) { break; }"
     )
   }
