aab5fe8924274d03674a02747dfdd823010ee0d9
Wenyao <38875149+shawenyao@users.noreply.github.com>
2020-04-09 16:10:19 -0400

    create my island map

--- main/animal_crossing/deserted_island_location.R
+++ main/animal_crossing/deserted_island_location.R
@@ -3,25 +3,27 @@ suppressWarnings(library(htmlwidgets))
 suppressWarnings(library(beepr))
 suppressWarnings(library(tidyverse))
 
-setwd("C:/Users/Wenyao/Desktop/R/R/")
+setwd("C:/Users/Wenyao/Desktop/R/R/output/animal_crossing/")
 
 
 #==== plot map ====
 output <- leaflet(
-  options = leafletOptions(minZoom = 3, maxZoom = 9)
+  options = leafletOptions(minZoom = 1, maxZoom = 6)
 ) %>% 
-  addProviderTiles(providers$Stamen.Toner) %>% 
-  setView(lng = -95, lat = 38, zoom = 4) %>% 
+  addProviderTiles(providers$Wikimedia) %>% 
+  setView(lng = 0, lat = 0, zoom = 2) %>% 
   setMaxBounds(lng1 = -180, lat1 = -90, lng2 = 180, lat2 = 90) %>%
   addRectangles(
-    lng1 = -118.456554, lat1 = 34.078039,
-    lng2 = -118.436383, lat2 = 34.062717,
-    fillColor = "transparent"
+    lng1 = -180, lat1 = 34,
+    lng2 = 180, lat2 = 35,
+    stroke = FALSE,
+    fillColor = "blue",
+    fillOpacity = 0.4
   )
 
 
 #===== save =====
-saveWidget(output, file = "map.html", selfcontained = TRUE)
+saveWidget(output, file = "my_island.html", selfcontained = TRUE)
 
 # play sound when finished
 beep(sound = 2)
