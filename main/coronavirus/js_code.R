d6e942fd3e8066996ef65986c3ae758c113f860c
Wenyao <38875149+shawenyao@users.noreply.github.com>
2020-05-07 17:57:31 -0400

    optimize JS code

--- main/coronavirus/js_code.R
+++ main/coronavirus/js_code.R
@@ -1,33 +1,17 @@
-# js code to let the animation start on load and loop for certain times
 js_code <- paste0(
+  
   "async function(el, x) {",
-  "await new Promise(r => setTimeout(r, 1000));",
-  "count = 0;",
-  "while(true){"
-)
-
-for(i in 1:(length(all_dates))){
+  "total_frames = ", length(all_dates), ";",
   
-  if(i != length(all_dates)){
-    current_frame <- length(all_dates) - i - 1
-  }else{
-    current_frame <- length(all_dates) - 1
-    
-    # additional wait on the last frame
-    js_code <- js_code %>% paste0(
-      "await new Promise(r => setTimeout(r, 1000));",
-      "if(count == 1 - 1) { break; }"
-    )
-  }
+  "var i;",
+  "for (i = 0; i < total_frames; i++) {",
+  "current_frame = total_frames - i - 1;",
   
-  js_code <- js_code %>% paste0(
-    # controls frames per second
-    "await new Promise(r => setTimeout(r, 50));",
-    "document.getElementsByTagName('input').item(", current_frame, ").click();"
-  )
-}
-
-js_code <- js_code %>% paste0(
-  "count = count + 1;",
-  "}}"
+  # controls frames per second
+  "await new Promise(r => setTimeout(r, 25));",
+  
+  "document.getElementsByTagName('input').item(current_frame).click();",
+  "}",
+  
+  "}"
 )
