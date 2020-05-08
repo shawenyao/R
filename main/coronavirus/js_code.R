169e3d9c665213ba04e635c56e85bf98cb4008f4
Wenyao <38875149+shawenyao@users.noreply.github.com>
2020-05-08 11:35:08 -0400

    optimize FPS

--- main/coronavirus/js_code.R
+++ main/coronavirus/js_code.R
@@ -3,12 +3,14 @@ js_code <- paste0(
   "async function(el, x) {",
   "total_frames = ", length(all_dates), ";",
   
+  "await new Promise(r => setTimeout(r, 1500));",
+  
   "var i;",
   "for (i = 0; i < total_frames; i++) {",
   "current_frame = total_frames - i - 1;",
   
   # controls frames per second
-  "await new Promise(r => setTimeout(r, 25));",
+  "await new Promise(r => setTimeout(r, 50));",
   
   "document.getElementsByTagName('input').item(current_frame).click();",
   "}",
