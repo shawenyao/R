7983894e20e1da0175b92917eadc184b4187bb87
Wenyao <38875149+shawenyao@users.noreply.github.com>
2020-10-11 20:19:09 -0400

    uniswap

---
+++ functions/functions_uniswap.R
@@ -0,0 +1,93 @@
+#' simulate geometric Brownian motion paths
+#' @param mu expected return
+#' @param sigma volatility
+#' @param dW a matrix of rnorms, useful when simulating with correlated processes
+#' @param price0 the initial price
+generate_paths <- function(mu, sigma, dW, price0, dt){
+  drift <- (mu - 0.5 * sigma^2) * dt
+  diffusion <- sigma * sqrt(dt) * dW
+  log_price_increment <- cbind(0, drift + diffusion)
+  log_price <- log(price0) + t(apply(log_price_increment, 1, cumsum))
+  exp(log_price)
+}
+
+
+#' simulate the returns of Uniswap liquidity providers
+#' based on asset pair A and B
+#' @param mu_A the expected return of asset A
+#' @param mu_B the expected return of asset B
+#' @param sigma_A the volatility of asset A
+#' @param sigma_B the volatility of asset B
+#' @param rho the correlation between asset A and B's returns
+#' @param fee the fee rate
+#' @param N the number of paths to simulate
+#' @param t the number of iterations per path
+#' @param dt the step size
+#' @param A0 the initial price of asset A
+#' @param B0 the initial price of asset B
+#' @param a0 the initial pool size of asset A
+#' @param b0 the initial pool size of asset B (a0 * A0 / B0)
+simulate_uniswap_returns <- function(
+  mu_A, mu_B,
+  sigma_A, sigma_B,
+  rho,
+  fee,
+  N, t, dt,
+  A0, B0,
+  a0, b0 = a0 * A0 / B0
+){
+  
+  # simulate price paths for asset A and B
+  dW_A <- matrix(rnorm(t / dt * N), nrow = N, ncol = t)
+  dW_B <- rho * dW_A + sqrt(1 - rho^2) * matrix(rnorm(t / dt * N), nrow = N, ncol = t)
+  
+  A <- generate_paths(
+    mu = mu_A, 
+    sigma = sigma_A, 
+    price0 = A0,
+    dW = dW_A,
+    dt = dt
+  )
+  B <- generate_paths(
+    mu = mu_B, 
+    sigma = sigma_B, 
+    price0 = B0,
+    dW = dW_B,
+    dt = dt
+  )
+  
+  # pool size
+  a <- matrix(nrow = N, ncol = t + 1)
+  b <- matrix(nrow = N, ncol = t + 1)
+  
+  # initial pool size
+  a[,1] <- a0
+  b[,1] <- b0
+  
+  # check the correlation in log price
+  # sapply(seq_len(N), function(i){cor(log(A[i,]), log(B[i,]))}) %>% summary()
+  
+  # evolution of pool balances
+  for(i in 2:(t+1)){
+    
+    a_prime <- sqrt(a[,i-1] * b[,i-1] * B[,i] / A[,i])
+    b_prime <- sqrt(a[,i-1] * b[,i-1] * A[,i] / B[,i])
+    
+    a[,i] <- a_prime + (a[,i-1] >= a_prime) * (a[,i-1] - a_prime) * fee
+    b[,i] <- b_prime + (a[,i-1] < a_prime) * (b[,i-1] - b_prime) * fee
+  }
+  
+  # initial value
+  v0 <- unique(a[,1] * A[,1] + b[,1] * B[,1])
+  
+  # strategy: provide liquidity
+  v_liquidity <- a[,t] * A[,t] + b[,t] * B[,t]
+  
+  # strategy: buy and hold
+  v_bah <- a[,1] * A[,t] + b[,1] * B[,t]
+  
+  v <- tibble(
+    `Liquidity Provider` = v_liquidity / v0,
+    `Buy and Hold` = v_bah / v0
+  )
+}
---
+++ main/uniswap/simulation.R
@@ -0,0 +1,179 @@
+library(tidyverse)
+library(latex2exp)
+
+setwd("C:/Users/Wenyao/Desktop/R/R/")
+source("./functions/functions_save_png.R")
+source("./functions/functions_uniswap.R")
+
+set.seed(1)
+
+#==== simulation ====
+v <- simulate_uniswap_returns(
+  mu_A = 0.001, mu_B = 0.002,
+  sigma_A = 0.02, sigma_B = 0.05,
+  rho = 0.8,
+  fee = 0.3 / 100,
+  N = 1e5, t = 1000, dt = 1,
+  A0 = 100, B0 = 200,
+  a0 = 20000
+)
+
+v_fee <- map(
+  c(0, 0.15, 0.3, 1, 5, 20) / 100,
+  function(fee){
+    print(fee)
+    simulate_uniswap_returns(
+      mu_A = 0.001, mu_B = 0.002,
+      sigma_A = 0.02, sigma_B = 0.05,
+      rho = 0.8,
+      fee = fee,
+      N = 1e5, t = 1000, dt = 1,
+      A0 = 100, B0 = 200,
+      a0 = 20000
+    ) %>% 
+      mutate(
+        fee = paste0(fee * 100, "%")
+      )
+  }
+) %>% 
+  bind_rows() %>% 
+  gather(
+    type, value, -fee
+  )
+
+v_rho <- map(
+  c(-0.5, 0, 0.5, 0.8, 0.9, 1),
+  function(rho){
+    print(rho)
+    simulate_uniswap_returns(
+      mu_A = 0.001, mu_B = 0.002,
+      sigma_A = 0.02, sigma_B = 0.05,
+      rho = rho,
+      fee = 0.3 / 100,
+      N = 1e5, t = 1000, dt = 1,
+      A0 = 100, B0 = 200,
+      a0 = 20000
+    ) %>% 
+      mutate(
+        rho = rho
+      )
+  }
+) %>% 
+  bind_rows() %>% 
+  gather(
+    type, value, -rho
+  )
+
+
+#==== plot =====
+plot1 <- v %>% 
+  gather(
+    type, value
+  ) %>%
+  ggplot(aes(x = value)) +
+  geom_density(aes(fill = type), alpha = 0.5, color = NA) +
+  geom_vline(xintercept = 0, alpha = 0.3, size = 1) +
+  scale_fill_manual(values = c("Liquidity Provider" = "tomato", "Buy and Hold" = "dodgerblue")) +
+  theme_minimal() +
+  theme(
+    legend.position = "top",
+    legend.title = element_blank(),
+    text = element_text(size = 25),
+    axis.title.x = element_text(margin = margin(t = 15, r = 0, b = 0, l = 0)),
+    axis.title.y = element_blank(),
+    axis.text.y = element_blank(),
+    plot.subtitle = element_text(size = 20)
+  ) +
+  labs(
+    title = "Return Distribution of Buy and Hold vs Liquidity Provider", 
+    subtitle = TeX('$\\mu^A = 0.1%,\\; \\mu^B = 0.2%,\\; \\sigma^A = 2%,\\; \\sigma^B = 5%,\\; \\rho = 0.8,\\; A_0 = 100,\\; B_0 = 200,\\; c = 0.3% $'),
+    x = "Return"
+  ) +
+  xlim(c(-0.5, 10))
+
+plot2 <- v_fee %>% 
+  ggplot(aes(x = value)) +
+  geom_density(aes(fill = type), alpha = 0.5, color = NA) +
+  geom_vline(xintercept = 0, alpha = 0.3, size = 1) +
+  scale_fill_manual(values = c("Liquidity Provider" = "tomato", "Buy and Hold" = "dodgerblue")) +
+  theme_minimal() +
+  theme(
+    legend.position = "top",
+    legend.title = element_blank(),
+    text = element_text(size = 25),
+    axis.title.x = element_text(margin = margin(t = 15, r = 0, b = 0, l = 0)),
+    axis.title.y = element_blank(),
+    axis.text.y = element_blank(),
+    plot.subtitle = element_text(size = 20)
+  ) +
+  facet_grid(fee~., switch = "y") +
+  labs(
+    title = "Return Distribution of Buy and Hold vs Liquidity Provider", 
+    subtitle = TeX('$ c = 0%,\\; 0.15%,\\; 0.3%,\\; 1%,\\; 5%,\\; 20% $'),
+    x = "Return"
+  ) +
+  xlim(c(-0.5, 10))
+
+plot3 <- v_rho %>%
+  ggplot(aes(x = value)) +
+  geom_density(aes(fill = type), alpha = 0.5, color = NA) +
+  geom_vline(xintercept = 0, alpha = 0.3, size = 1) +
+  scale_fill_manual(values = c("Liquidity Provider" = "tomato", "Buy and Hold" = "dodgerblue")) +
+  theme_minimal() +
+  theme(
+    legend.position = "top",
+    legend.title = element_blank(),
+    text = element_text(size = 25),
+    axis.title.x = element_text(margin = margin(t = 15, r = 0, b = 0, l = 0)),
+    axis.title.y = element_blank(),
+    axis.text.y = element_blank(),
+    plot.subtitle = element_text(size = 20)
+  ) +
+  facet_grid(rho~., switch = "y") +
+  labs(
+    title = "Return Distribution of Buy and Hold vs Liquidity Provider", 
+    subtitle = TeX('$ \\rho = -0.5,\\; 0,\\; 0.5,\\; 0.8,\\; 0.9,\\; 1 $'),
+    x = "Return"
+  ) +
+  xlim(c(-0.5, 10))
+
+
+#===== save =====
+save_png(
+  plot1,
+  file_name = "output/uniswap/return.png",
+  width = 800,
+  height = 400
+)
+
+save_png(
+  plot2,
+  file_name = "output/uniswap/return_vs_fees.png",
+  width = 800,
+  height = 900
+)
+
+save_png(
+  plot3,
+  file_name = "output/uniswap/return_vs_rhos.png",
+  width = 800,
+  height = 900
+)
+
+v_fee %>% 
+  group_by(fee, type) %>% 
+  summarise(
+    expected_return = mean(value),
+    volatility = sd(value),
+    sharpe_ratio = expected_return / volatility
+  ) %>% 
+  View()
+
+v_rho %>% 
+  group_by(rho, type) %>% 
+  summarise(
+    expected_return = mean(value),
+    volatility = sd(value),
+    sharpe_ratio = expected_return / volatility
+  ) %>% 
+  View()
