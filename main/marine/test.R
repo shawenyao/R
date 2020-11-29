30d62b9e3bb99bb01e272ddb12ed2e6c6525da8c
Wenyao <38875149+shawenyao@users.noreply.github.com>
2020-11-28 22:48:52 -0500

    Update test.R

--- main/marine/test.R
+++ main/marine/test.R
@@ -16,7 +16,6 @@ ships <- import("input/ships.RData")
 #==== test ====
 # one and only one longest observation is allowed per `SHIP_ID`
 expect_uniqueness <- function(df) {
-  
   longest_count <- df %>% 
     group_by(SHIP_ID) %>% 
     summarise(longest_count = sum(ind_longest, na.rm = TRUE)) %>% 
