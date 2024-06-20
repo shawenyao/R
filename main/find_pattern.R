b027009446d781b45c5dae8f23a0a23eeebb594c
Wenyao <38875149+shawenyao@users.noreply.github.com>
2024-06-20 16:49:45 -0700

    Update find_pattern.R

--- main/find_pattern.R
+++ main/find_pattern.R
@@ -6,13 +6,6 @@ for(i in 1:ceiling(sqrt(n))^2){
     x <- 1
     y <- 1
     direction <- 'down'
-  }else if(direction == 'right'){
-    y <- y + 1
-    if(x == 1){
-      direction <- 'down'
-    }else if(y == x){
-      direction <- 'up'
-    }
   }else if(direction == 'down'){
     x <- x + 1
     if(x == y){
@@ -20,15 +13,22 @@ for(i in 1:ceiling(sqrt(n))^2){
     }else if(y == 1){
       direction <- 'right'
     }
-  }else if(direction == 'left'){
-    y <- y - 1
-    if(y == 1){
+  }else if(direction == 'right'){
+    y <- y + 1
+    if(x == 1){
       direction <- 'down'
+    }else if(y == x){
+      direction <- 'up'
     }
   }else if(direction == 'up'){
     x <- x - 1
     if(x == 1){
       direction <- 'right'
+    }
+  }else if(direction == 'left'){
+    y <- y - 1
+    if(y == 1){
+      direction <- 'down'
     }
   }
   
@@ -37,3 +37,4 @@ for(i in 1:ceiling(sqrt(n))^2){
 
 View(m)
 which(m==n, arr.ind = T)
+  
