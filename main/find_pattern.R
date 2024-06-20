00d58c5de29a841c2f7e6e4cf15fb0bdb8ebd24f
Wenyao <38875149+shawenyao@users.noreply.github.com>
2024-06-20 15:48:40 -0700

    Create find_pattern.R

---
+++ main/find_pattern.R
@@ -0,0 +1,41 @@
+n <- 2019
+
+m <- matrix(nrow = ceiling(sqrt(n)), ncol = ceiling(sqrt(n)))
+
+
+for(i in 1:ceiling(sqrt(n))^2){
+  if(i == 1){
+    x <- 1
+    y <- 1
+    direction <- 'down'
+  }else if(direction == 'right'){
+    y <- y + 1
+    if(x == 1){
+      direction <- 'down'
+    }else if(y == x){
+      direction <- 'up'
+    }
+  }else if(direction == 'down'){
+    x <- x + 1
+    if(x == y){
+      direction <- 'left'
+    }else if(y == 1){
+      direction <- 'right'
+    }
+  }else if(direction == 'left'){
+    y <- y - 1
+    if(y == 1){
+      direction <- 'down'
+    }
+  }else if(direction == 'up'){
+    x <- x - 1
+    if(x == 1){
+      direction <- 'right'
+    }
+  }
+  
+  m[x, y] <- i
+}
+
+View(m)
+which(m==2019, arr.ind = T)
